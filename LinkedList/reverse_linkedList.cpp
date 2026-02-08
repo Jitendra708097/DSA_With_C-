8774e8887245ccdd92ed8016571db16556e1a375
Jitendra Sharma <jitendravishvakarma708097@gmail.com>
2026-02-08 15:10:47 +0530

    new file:   reverse_linkedList.cpp

---
+++ LinkedList/reverse_linkedList.cpp
@@ -0,0 +1,81 @@
+// Reverse Linked list 
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+// print linked list 
+void printLinkedList(Node *temp)
+{
+    if(temp == NULL)
+    return ;
+
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+};
+
+// reverse linked list 
+Node *reverse(Node *head)
+{
+    if(head == NULL)
+    return 0;
+
+    Node *temp,*prev,*nextPointer;
+    temp = head;
+    prev = nullptr;
+
+    while (temp != NULL)
+    {
+        nextPointer = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = nextPointer;
+    }
+
+    return prev;
+    
+}
+
+int main()
+{
+   int arr[5] = {1,2,3,4,5};
+   Node *head = NULL;
+   Node *temp;
+
+for(int i=0;i<5;i++)
+{
+   if(head == NULL)
+   {
+    head = new Node(arr[i]);
+    temp = head;
+   }
+   else
+   {
+    Node *tail = new Node(arr[i]);
+    temp->next = tail;
+    temp = tail;
+   }
+}
+
+cout<<"Print linked list: ";
+printLinkedList(head);
+
+cout<<endl<<"Reverse Linked List: ";
+Node *temp1 = reverse(head);
+printLinkedList(temp1);
+
+}
